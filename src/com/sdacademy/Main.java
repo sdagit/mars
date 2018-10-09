9b54f770a0e6bf46691808f792c2bf7b9d9a3d9e
Karina Wilk desktop <sda_git@protonmail.com>
2018-10-09 23:55:32 +0200

    Szkielet aplikacji

---
+++ src/com/sdacademy/Main.java
@@ -0,0 +1,20 @@
+package com.sdacademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Podaj liczbę do przekonwertowania");
+
+        //TODO: Odczytaj liczbę całkowitą.
+
+        System.out.println("Podaj podstawę systemu wyjścioweg (2 dla binarnego, 8 dla ósemkowego lub 16 dla hexa");
+
+        //TODO: ODczytaj liczbę całkowitą. Jeśli jest inna niż 2, 8 lub 16, zakończ aplikację.
+
+        System.out.println("Podana liczba w wybranym systemie ma postać:");
+
+        //TODO W zależności od podstawy systemu (2, 8, czy 16), użyć odpowiedniej metdody i wyświetlić metodę konwersji.
+
+    }
+}
